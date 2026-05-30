47cfe8248a719eddb882ae8c1d0a1edc028bf44c
Shashwat Srivastava <pymagic140@gmail.com>
2026-05-30 14:19:34 +0530

    Fix 404 detection by changing HEAD to GET

--- netlify-redirector/functions/go.js
+++ netlify-redirector/functions/go.js
@@ -73,7 +73,6 @@ export default async (request, context) => {
   if (finalAmazonUrl.includes("/dp/")) {
     try {
       const amzRes = await fetch(finalAmazonUrl, {
-        method: "HEAD",
         headers: {
           "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
         }
